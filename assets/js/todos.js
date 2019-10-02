c2679cdf3509f05f423131a93150e6fe5b5e4895
Ralph <ralph.lee@live.ca>
2019-10-02 02:05:41 +0000

    Add adding and removing functions

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,5 @@
 // Check off specific Todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   
   // // if li is gray
   //   //turn it black
@@ -20,3 +20,24 @@ $("li").click(function() {
   $(this).toggleClass("completed");
   
 });
+
+//Click on X to delete a Todo
+
+$("ul").on("click", "span", function(event) {
+  event.stopPropagation();
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  })
+})
+
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+    //Grab new todo text from input
+    var todoText = $(this).val();
+
+    $(this).val("");
+
+    //Create new li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>")
+  }
+})
