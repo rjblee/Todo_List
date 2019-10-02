55f660f68256faa0399474ce91426f02ceb52b39
Ralph <ralph.lee@live.ca>
2019-10-02 00:15:53 +0000

    Add basic HTML

---
+++ assets/js/todos.js
@@ -0,0 +1,20 @@
+// Check off specific Todos by clicking
+$("li").click(function() {
+  
+  // if li is gray
+    //turn it black
+  //else
+    //turn it grey
+  if($(this).css("color") === "rgb(128, 128, 128)") {
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
+  } else {
+    $(this).css({
+      color: "grey",
+      textDecoration: "line-through"
+    });
+  }
+  
+});
