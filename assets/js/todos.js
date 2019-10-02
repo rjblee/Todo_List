6c9ce675ce52b1b9f0d9cd7d7790375e1358bd01
Ralph <ralph.lee@live.ca>
2019-10-02 06:45:16 +0000

    Add icons and gradients

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -38,6 +38,6 @@ $("input[type='text']").keypress(function(event) {
     $(this).val("");
 
     //Create new li and add to ul
-    $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
   }
 })
